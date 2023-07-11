2d39700e8340b91301c42561d1ea3807dbbda0b5
Daniel Alejandro Leyva Perez <dalejandrolp99@gmail.com>
2023-07-11 16:05:40 -0400

    feat: switch from cloud to local supabase for development speed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
-export default function Home() {
+export default async function Home() {
+  var Odoo = require("async-odoo-xmlrpc");
+  var odoo = new Odoo({
+    url: "http://localhost",
+    port: "8110",
+    db: "test",
+    username: "admin",
+    password: "admin",
+  });
+  const test = await odoo.connect();
+  console.log(test);
+  let id = await odoo.execute_kw("account.invoice", "search", [[]]);
   return (
     <main>
-      <h1>Odoo client</h1>
+      <h1>{test}</h1>
     </main>
   );
 }
