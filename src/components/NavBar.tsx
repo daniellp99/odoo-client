9577156bfd4d3551a3fbe443e3d48836818cb80a
Daniel Alejandro Leyva Perez <dalejandrolp99@gmail.com>
2023-07-23 09:59:29 -0400

    style: change name site color text

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -14,7 +14,7 @@ export function MainNav({ items }: MainNavProps) {
     <div className="flex gap-6 md:gap-10">
       <Link href="/" className="flex items-center space-x-2">
         <Icons.logo className="h-6 w-6" />
-        <span className="inline-block font-bold">Odoo Client</span>
+        <span className="inline-block font-bold text-background">Odoo Client</span>
       </Link>
       {items?.length ? <NavMenu items={items} /> : null}
     </div>
