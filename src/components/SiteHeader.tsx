a00fe4256b98c82c3f5eef556fdafc9788e2c72b
Daniel Alejandro Leyva Perez <dalejandrolp99@gmail.com>
2023-07-23 09:58:38 -0400

    style: change bg color and height of siteHeader

--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -15,8 +15,8 @@ export async function SiteHeader() {
   const session = await getAuthSession();
 
   return (
-    <header className="bg-background sticky top-0 z-40 w-full border-b">
-      <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
+    <header className="bg-primary sticky top-0 z-40 w-full border-b">
+      <div className="container flex h-12 items-center space-x-4 sm:justify-between sm:space-x-0">
         <MainNav items={siteConfig.mainNav} />
         <div className="flex flex-1 items-center justify-end space-x-4">
           {session?.user ? (
