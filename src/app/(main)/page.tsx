ff9bf24deb9f3e7d861e60dd435bb83d01afbf48
Daniel Alejandro Leyva Perez <dalejandrolp99@gmail.com>
2023-07-27 10:54:09 -0400

    refactor: remove connection test from main page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,21 +1,9 @@
 import { Button } from "@/components/ui/Button";
 
 export default async function Home() {
-  var Odoo = require("async-odoo-xmlrpc");
-  var odoo = new Odoo({
-    url: "http://localhost",
-    port: "8110",
-    db: "test",
-    username: "admin",
-    password: "admin",
-  });
-  const test = await odoo.connect();
-  console.log(test);
-  let id = await odoo.execute_kw("account.invoice", "search", [[]]);
   return (
     <main>
       <Button className="animate-in zoom-in duration-500">click</Button>
-      <h1>{test} u</h1>
     </main>
   );
 }
